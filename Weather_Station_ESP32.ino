f86ff7a6febbfeb7a63e8ffaf5a208cb82183c35
nojus-j <nojjon@ktu.lt>
2026-04-02 19:25:48 +0300

    Update Weather_Station_ESP32.ino

--- Weather_Station_ESP32.ino
+++ Weather_Station_ESP32.ino
@@ -1,9 +1,34 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+#define BMP280_ADDRESS 0x76
+Adafruit_BMP280 bmp;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) delay(100);
+  Serial.println("BMP280 test");
+  unsigned status;
+  status = bmp.begin(BMP280_ADDRESS);
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, // Operating mode
+                  Adafruit_BMP280::SAMPLING_X2, // Temperature oversampling
+                  Adafruit_BMP280::SAMPLING_X16, // Pressure oversampling
+                  Adafruit_BMP280::FILTER_X16, // Filtering
+                  Adafruit_BMP280::STANDBY_MS_500); // Standby time
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" m");
+  delay(1000);
 }
